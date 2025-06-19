161a4b4fec4ecc540fd0cf8a32916500a54cd4b6
Georgelxll <166880649+Georgelxll@users.noreply.github.com>
2025-06-19 07:59:27 -0300

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const config = require("./config.json");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const prefix = "!";
+
+client.on("messageCreate", function (message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+});
+
+client.login(config.BOT_TOKEN);
